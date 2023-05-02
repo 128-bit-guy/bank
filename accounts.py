ccebae24441db42c2165a8ff4b9386c5e73beee7
128-bit-guy <44749584+128-bit-guy@users.noreply.github.com>
2023-05-02 21:28:10 +0300

    Add basic account handling

---
+++ accounts.py
@@ -0,0 +1,8 @@
+class Account:
+    def __init__(self, name, password, balance):
+        self.name = name
+        self.password = password
+        self.balance = balance
+
+
+accounts = {}
--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, session
+from accounts import *
 app = Flask("Bank")
+app.secret_key = "admin"
+
+
+def handle_account():
+    if request.method == 'POST':
+        if 'log_out' in request.form:
+            del session["login"]
+            return None
+        else:
+            login = request.form['login']
+            password = request.form['password']
+            if 'log_in' in request.form:
+                if login in accounts:
+                    if password == accounts[login].password:
+                        session["login"] = login
+                        return None
+                    else:
+                        return "Invalid password!"
+                else:
+                    return "User does not exist!"
+            else:
+                if login in accounts:
+                    return "User already exists!"
+                else:
+                    accounts[login] = Account(login, password, 0)
+                    session["login"] = login
+                    return None
 
 
 @app.route("/", methods=['GET', 'POST'])
 def draw_index():
-    if request.method == 'POST':
-        login = request.form['login']
-        password = request.form['password']
-        log_in = 'log_in' in request.form
-        return login + " " + password + " " + str(log_in)
-    return render_template("index.html")
+    x = handle_account()
+    if x is not None:
+        return x
+    elif "login" in session:
+        return render_template("index.html", logged_in=True, login=session["login"])
+    else:
+        return render_template("index.html", logged_in=False)
 
 
 app.run()
