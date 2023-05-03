3f936930248fa1e6526ec77309b21b15899b9669
128-bit-guy <44749584+128-bit-guy@users.noreply.github.com>
2023-05-03 16:09:42 +0300

    Add send money page

--- accounts.py
+++ accounts.py
@@ -3,6 +3,16 @@ class Account:
         self.name = name
         self.password = password
         self.balance = balance
+        self.unlimited_money = False
+
+    def send_money(self, money, other):
+        if not self.unlimited_money and money > self.balance:
+            return False
+        self.balance -= money
+        other.balance += money
+        return True
 
 
-accounts = {}
+accounts = {"admin": Account("admin", "admin", 0)}
+
+accounts["admin"].unlimited_money = True
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session
 from accounts import *
+
 app = Flask("Bank")
 app.secret_key = "admin"
 
@@ -54,7 +55,32 @@ def draw_account():
         if 'change_password' in request.form:
             accounts[session["login"]].password = request.form['password']
         return render_template("account.html", logged_in=True, login=session["login"],
-                               balance=accounts[session["login"]].balance)
+                               balance="Неограниченно" if accounts[session["login"]].unlimited_money else accounts[
+                                   session["login"]].balance)
+    else:
+        return render_template("error.html", error="Для того чтобы пользоваться этой страницой необходимо войти в "
+                                                   "учётную запись")
+
+
+@app.route("/send_money", methods=['GET', 'POST'])
+def draw_send_money():
+    x = handle_account()
+    if x is not None:
+        return x
+    elif "login" in session:
+        if 'send_money' in request.form:
+            user = request.form['user']
+            money_count = request.form['money_count']
+            if not money_count.isnumeric():
+                return render_template("error.html", error="Количество денег должно быть числом")
+            money_count = int(money_count)
+            if money_count <= 0:
+                return render_template("error.html", error="Количество денег должно быть положительным")
+            if user not in accounts:
+                return render_template("error.html", error="Заданного пользователя не существует")
+            if not accounts[session["login"]].send_money(money_count, accounts[user]):
+                return render_template("error.html", error="Недостаточно средств")
+        return render_template("send_money.html", logged_in=True, login=session["login"])
     else:
         return render_template("error.html", error="Для того чтобы пользоваться этой страницой необходимо войти в "
                                                    "учётную запись")
